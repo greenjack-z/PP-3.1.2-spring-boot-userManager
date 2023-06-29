b015427f8e6f78ffe3aab4d96409ccf3c8424812
greenjack <e.zelyanin@gmail.com>
2023-06-29 13:17:59 +0300

    Initial commit

---
+++ src/test/java/edu/greenjack/usermanager/UserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenjack.usermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
