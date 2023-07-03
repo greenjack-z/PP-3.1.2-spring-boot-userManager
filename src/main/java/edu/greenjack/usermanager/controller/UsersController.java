b9a1c73ef936f02afd792cecaaea63ae8f6bca7a
greenjack <e.zelyanin@gmail.com>
2023-07-03 19:38:52 +0300

    Workable configuration

--- src/main/java/edu/greenjack/usermanager/controller/UsersController.java
+++ src/main/java/edu/greenjack/usermanager/controller/UsersController.java
@@ -1,13 +1,11 @@
-package edu.greenjack.usermanager.web;
+package edu.greenjack.usermanager.controller;
 
 
 import edu.greenjack.usermanager.model.User;
 import edu.greenjack.usermanager.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,13 +35,13 @@ public class UsersController {
         return REDIRECT_USERS;
     }
 
-    @PostMapping("/users/update")
+    @PatchMapping("/users/update")
     public String updateUser(@ModelAttribute User user) {
         usersService.updateUser(user);
         return REDIRECT_USERS;
     }
 
-    @PostMapping("/users/delete")
+    @DeleteMapping("/users/delete")
     public String deleteUser(@ModelAttribute User user) {
         usersService.deleteUser(user);
         return REDIRECT_USERS;
