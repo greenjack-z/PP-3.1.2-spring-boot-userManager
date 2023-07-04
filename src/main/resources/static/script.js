54acf9150288c4fb14c615d4d6d0c644ae8cebec
greenjack <e.zelyanin@gmail.com>
2023-07-04 14:25:02 +0300

    Move js to external. Change the hidden _method by onclick

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,6 @@
+
+function rowClicked(id, name, age) {
+    document.getElementById("id").value = id;
+    document.getElementById("name").value = name;
+    document.getElementById("age").value = age;
+}
